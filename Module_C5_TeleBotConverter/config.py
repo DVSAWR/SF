fa4a8b955b601b64c4ce297c3220a1d3810f11e6
Gruzdev Daniil <gruzdev.daniil@gmail.com>
2023-06-01 20:16:03 +0300

    add new file (TeleBot)

---
+++ Module_C5_TeleBotConverter/config.py
@@ -0,0 +1,14 @@
+exchanges = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'юань': 'CNY',
+    'лари': 'GEL',
+    'злотый': 'PLN',
+    'лира': 'TRY',
+    'риал': 'IRR',
+    'рупия': 'INR',
+    'шекель': 'ILS'
+}
+
+TOKEN = '6204515186:AAG7UDox61aEPrB8nsOTixUN13cwzX07VjY'
